67868deeb694ec1a8ab4e229c0aae84d9aec4f55
Mikayla <user@users-MacBook-Pro.local>
2023-07-18 11:33:41 -0400

    established proper port connection and sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+console.log(process.env);
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-    });
-
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 8889
+  });
+}
 module.exports = sequelize;
